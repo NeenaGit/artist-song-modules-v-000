f8e082890407f9824662a17e8563c1dac45587c8
Neena van de Water <nkuriakose@hotmail.com>
2018-06-14 18:12:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,16 +10,17 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  extend Findable
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
   extend Memorable
-  
+
   def to_param
     name.downcase.gsub(' ', '-')
   end
